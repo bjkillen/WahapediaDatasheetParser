d3443dfca6c2e4a0b3ecd6134472585577a15bd7
bjkillen <blaine.j.killen@gmail.com>
2024-09-23 12:32:51 -0700

    updated packae and increase ease of use of command args

--- src/commands/ParseModelsDatasheetsAndWargear.ts
+++ src/commands/ParseModelsDatasheetsAndWargear.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 
 import { Args, Command } from "@oclif/core";
+import path from "path";
 
 import ModelDatasheetParser from "../models/ModelDatasheetParser";
 import WargearParser from "../models/WargearParser";
@@ -11,18 +12,17 @@ import KeywordParser from "../models/DatasheetKeywordsParser";
 
 export default class ParseModelsDatasheetsAndWargear extends Command {
 	static override args = {
-		factionsSrcPath: Args.string(),
-		unitDatasheetsSrcPath: Args.string(),
-		modelDatasheetsSrcPath: Args.string(),
-        wargearSrcPath: Args.string(),
-		keywordsSrcPath: Args.string()
+		srcDir: Args.string()
 	};
 
 	public async run(): Promise<void> {
 		const { args } = await this.parse(ParseModelsDatasheetsAndWargear);
 
-		let unitDatasheetResults = await UnitDatasheetParser.ParseFile(args.unitDatasheetsSrcPath);
-        let modelDatasheetResults = await ModelDatasheetParser.ParseFile(args.modelDatasheetsSrcPath);
+		let unitDatasheetResults = await UnitDatasheetParser.ParseFile(
+			path.join(args.srcDir, 'Datasheets.csv'));
+
+        let modelDatasheetResults = await ModelDatasheetParser.ParseFile(
+			path.join(args.srcDir, 'Datasheets_models.csv'));
 
 		modelDatasheetResults.forEach((modelDatasheet) => {
 			if (unitDatasheetResults.has(modelDatasheet.datasheetId)) {
@@ -30,7 +30,8 @@ export default class ParseModelsDatasheetsAndWargear extends Command {
 			}
 		})
 
-		const wargearResults = await WargearParser.ParseFile(args.wargearSrcPath);
+		const wargearResults = await WargearParser.ParseFile(
+			path.join(args.srcDir, 'Datasheets_wargear.csv'));
 
 		wargearResults.forEach((wargear) => {
 			if (unitDatasheetResults.has(wargear.datasheetId)) {
@@ -38,7 +39,8 @@ export default class ParseModelsDatasheetsAndWargear extends Command {
 			}
 		});
 
-		const keywordsResults = await KeywordParser.ParseFile(args.keywordsSrcPath);
+		const keywordsResults = await KeywordParser.ParseFile(
+			path.join(args.srcDir, 'Datasheets_keywords.csv'));
 
 		keywordsResults.forEach((keyword) => {
 			if (unitDatasheetResults.has(keyword.datasheetId)) {
@@ -46,7 +48,8 @@ export default class ParseModelsDatasheetsAndWargear extends Command {
 			}
 		});
 
-		const factionResults = await FactionParser.ParseFile(args.factionsSrcPath);
+		const factionResults = await FactionParser.ParseFile(
+			path.join(args.srcDir, 'Factions.csv'));
 
 		unitDatasheetResults.forEach((unitDatasheet) => {
 			if (factionResults.has(unitDatasheet.factionId)) {
